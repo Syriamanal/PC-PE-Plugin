bc84ec0941c954a8ed805279ad25a9b8c9fa63df
yale2a1 <yale2a1@naver.com>
2014-02-03 12:32:12 +0900

    플러그인파일내 오타수정 정상작동함

--- MARS.php
+++ MARS.php
@@ -1,34 +1,40 @@
 <?php
 /*
 __PocketMine Plugin__
-name:Mars
+name=Mars
 description=리무의 이름을 말할시 운지(하지만 제작자는 언급해도 안전는 구라)
 version=0.1.1
 author=Betta_Fish
 class=OWNNJI
-apiversion=11
+apiversion=12
 */
+
 
 class OWNNJI implements Plugin{
 	private $api;
-	
+
+
 	public function __construct(ServerAPI $api, $server = false){
 		$this->api = $api;
 	}
-	
+
+
 	public function init(){
 		$this->api->addHandler("player.chat", array($this, "handler"));
 	}
-	
+
+
 	public function __destruct(){}
-	
+
+
 	public function handler($data, $event){
 		switch ($event){
 			case "player.chat":
 				//player.chat이라면, 이부분이 실행됨
 				$player = $data['player'];
 				$msg = $data['message'];
-				
+
+
 				if(strpos($msg,'화성||이화성||Mars||마스') !==false){
 					$this->api->chat->broadcast($player->username." 운지!");
 					$player->teleport(new Vector3(20,300,20));
--- OPPLAYER.php
+++ OPPLAYER.php
@@ -1,36 +1,40 @@
 <?php
 /*
 __PocketMine Plugin__
-name:OP,Player
-description=구분합니다
+name=OP,Player
+description=op and player distinction
 version=0.1.1
 author=Betta_Fish
 class=OP
-apiversion=11
+apiversion=12
 */
+
 
 class OP implements Plugin{
 	private $api;
-	
+
+
 	public function __construct(ServerAPI $api, $server = false){
 		$this->api = $api;
 	}
-	
+
+
 	public function init(){
 		$this->api->addHandler("player.chat", array($this, "handler"));
 	}
-	
+
+
 	public function __destruct(){}
-	
+
+
 	public function handler(){
 		$player = $data["player"]; 
 		if($this->api->isOp(player){ 
-			$this->api->chat->broadcast("[Admin.]".$data["player"]->username." | ".$data["message"]);
+			$this->api->chat->broadcast("[ADMIN]".$data["player"]->username." | ".$data["message"]);
 		}else{
-			$this->api->chat->broadcast("[Player]".$data["player"]->username." | ".$data["message"]);
+			$this->api->chat->broadcast("[PLAYER]".$data["player"]->username." | ".$data["message"]);
 		}
 		return false:
 	}
 }
 ?>
-		
