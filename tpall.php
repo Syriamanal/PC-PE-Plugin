a5fcb3b9cc6f23ed17d23c3cbc90b418cca00a62
키리토 <nno88551@naver.com>
2014-02-03 15:19:55 +0900

    OCD and bug fix

--- tpall.php
+++ tpall.php
@@ -11,35 +11,32 @@ apiversion=10
 */
 
 class tp implements Plugin{
-    private $api;
-    public function __construct(ServerAPI $api, $server = false)
-    {
-        $this->api = $api;
-    }
-
+	private $api;
+	public function __construct(ServerAPI $api, $server = false){
+		$this->api = $api;
+	}
+	
+	public function __destruct(){}
     public function init(){
         $this->api->console->register("tpall","Tp's all the online players to your position", array($this, "tp"));
     }
 
-    public function tp($cmd, $args, $issuer)
-    	{
-    		if(!$issuer instanceof player){
-			return console("please run this command in-game.");
-			}
-			if($this->api->isOp(player)
-			{
-        		$x = $issuer->entity->x;
-        		$y = $issuer->entity->y;
-        		$z = $issuer->entity->z;
-        		$level = $issuer->level;
-        		foreach($this->api->player->getAll() as $player){
-            	$player->teleport(new Position($x, $y, $z, $level));
-			}
-        		return "[tpall]모든 인원이 tp 됩니다.";
-			}else{
-			
+    public function tp($cmd, $args, $issuer){
+		if(!$issuer instanceof player){
+			return("please run this command in-game.");
 		}
-
-    public function __destruct(){}
+		if($this->api->isOp(player){
+        	$x = $issuer->entity->x;
+        	$y = $issuer->entity->y;
+        	$z = $issuer->entity->z;
+        	$level = $issuer->level;
+        	foreach($this->api->player->getAll() as $player){
+           		$player->teleport(new Position($x, $y, $z, $level));
+			}
+        	return "[tpall]모든 인원이 tp 됩니다.";
+		}else{
+				
+		}
+	}
 }
 ?>
